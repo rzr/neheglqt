f1d17d9e5b4016384484c3df9a16da51308afbf9
Philippe Coval <rzr@gna.org>
2011-06-30 10:52:05 +0200

    fix: qt3 relics

--- ch11/main.cpp
+++ ch11/main.cpp
@@ -1,14 +1,19 @@
-#include <qapplication.h>
+#include "config.h"
+
+#include <QApplication>
+#include <QDir>
 
 #include "ch11.h"
 
 int main( int argc, char **argv )
 {
-	QApplication a( argc, argv );
-	
-	NeHeWidget *w = new NeHeChapter11();
-	a.setMainWidget( w );
-	w->show();
-	
-	return a.exec();
+  QApplication a( argc, argv );
+
+  QString dir = QCoreApplication::applicationDirPath();
+  QDir::setCurrent( dir );
+
+  NeHeChapter11 w;
+  w.show();
+
+  return a.exec();
 }
